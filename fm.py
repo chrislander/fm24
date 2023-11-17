3d425c3bfa415bd9d5ddb2e34012921b023c7c8c
Chris Lander <chrislander@gmail.com>
2023-11-17 23:43:49 +0000

    positions list finished

--- fm.py
+++ fm.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from io import StringIO
+import os
+from positions_list import positions
 
 def squad_html_to_dataframe(html_content):
     # Use StringIO to create a file-like object from the HTML string
@@ -16,14 +18,42 @@ def squad_html_to_dataframe(html_content):
         else:
             df[column].fillna(0.0, inplace=True)  # For numeric type columns, fill with 0.0
 
-    return df
+    return df    
 
-with open('exports/squad.html', 'r', encoding='UTF-8') as file:
+# Function to get the most recent file in a directory
+def get_latest_file(directory, file_extension):
+    # List all files in the directory with the specified extension
+    files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(file_extension)]
+    # Sort files by modification time, latest first
+    latest_file = max(files, key=os.path.getmtime)
+    return latest_file
+
+# Use the function to get the latest .html file
+latest_file_path = get_latest_file('exports/', '.html')
+
+# Read the content from the latest file
+with open(latest_file_path, 'r', encoding='UTF-8') as file:
     html_content = file.read()
 
+# Existing code remains the same for processing the HTML content
 df = squad_html_to_dataframe(html_content)
-
 print(df)
 
 for column in df.columns:
     print(column)
+
+number_of_positions = len(positions)
+
+print("Total number of positions:", number_of_positions)
+
+
+'''
+
+for position, attributes in positions.items():
+    key_score = sum(squad_rawdata[attr] for attr in attributes["key"])
+    green_score = sum(squad_rawdata[attr] for attr in attributes["green"])
+    blue_score = sum(squad_rawdata[attr] for attr in attributes["blue"])
+    total_score = (key_score * 5 + green_score * 3 + blue_score) / <appropriate divisor>
+    squad_rawdata[f'{position.replace(" ", "_").lower()}_score'] = total_score.round(1)
+'''
+
---
+++ positions_list.py
@@ -0,0 +1,423 @@
+positions = {
+    "Goalkeeper - Defend": {
+        "key": ["Agi", "Ref"],
+        "green": ["Aer", "Cmd", "Han", "Kic", "Cnt", "Pos"],
+        "blue": ["1v1", "Thr", "Ant", "Dec"]
+    },
+    "Sweeper Keeper - Defend": {
+        "key": ["Agi", "Ref"],
+        "green": ["Cmd", "Kic", "1v1", "Ant", "Cnt", "Pos"],
+        "blue": ["Aer", "Fir", "Han", "Pas", "TRO", "Dec", "Vis", "Acc"]
+    },
+    "Sweeper Keeper - Support": {
+        "key": ["Agi", "Ref"],
+        "green": ["Cmd", "Kic", "1v1", "Ant", "Cnt", "Pos"],
+        "blue": ["Aer", "Fir", "Han", "Pas", "TRO", "Dec", "Vis", "Acc"]
+    },
+    "Sweeper Keeper - Attack": {
+        "key": ["Agi", "Ref"],
+        "green": ["Cmd", "Kic", "1v1", "Ant", "Cnt", "Pos"],
+        "blue": ["Aer", "Fir", "Han", "Pas", "TRO", "Dec", "Vis", "Acc"]
+    },
+    "Ball Playing Defender - Defend": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Mar", "Pas", "Tck", "Pos", "Str"],
+        "blue": ["Fir", "Tec", "Agg", "Ant", "Bra", "Cnt", "Dec", "Vis"]
+    },
+    "Ball Playing Defender - Stopper": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Pas", "Tck", "Pos", "Str", "Agg", "Bra", "Dec"],
+        "blue": ["Fir", "Tec", "Ant", "Cnt", "Vis", "Mar"]
+    },
+    "Ball Playing Defender - Cover": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Mar", "Pas", "Tck", "Pos", "Ant", "Cnt", "Dec"],
+        "blue": ["Fir", "Tec", "Bra", "Vis", "Str", "Hea"]
+    },
+    "Central Defender - Defend": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Mar", "Tck", "Pos", "Str"],
+        "blue": ["Agg", "Ant", "Bra", "Cnt", "Dec"]
+    },
+       "Central Defender - Stopper": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Tck", "Agg", "Bra", "Dec", "Pos", "Str"],
+        "blue": ["Mar", "Ant", "Cnt"]
+    },
+    "Central Defender - Cover": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Mar", "Tck", "Ant", "Cnt", "Dec", "Pos"],
+        "blue": ["Hea", "Bra", "Str"]
+    },
+    "Complete Wing Back - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Tec", "OtB", "Tea"],
+        "blue": ["Fir", "Mar", "Pas", "Tck", "Ant", "Dec", "Fla", "Pos", "Agi", "Bal"]
+    },
+    "Complete Wing Back - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Tec", "Fla", "OtB", "Tea"],
+        "blue": ["Fir", "Mar", "Pas", "Tck", "Ant", "Dec", "Pos", "Agi", "Bal"]
+    },
+    "Full Back - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Mar", "Tck", "Ant", "Cnt", "Pos"],
+        "blue": ["Cro", "Pas", "Dec", "Tea"]
+    },
+    "Full Back - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Mar", "Tck", "Ant", "Cnt", "Pos", "Tea"],
+        "blue": ["Cro", "Dri", "Pas", "Tec", "Dec"]
+    },
+    "Full Back - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Mar", "Tck", "Ant", "Pos", "Tea"],
+        "blue": ["Dri", "Fir", "Pas", "Tec", "Cnt", "Dec", "OtB", "Agi"]
+    },
+    "Inverted Full Back - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Hea", "Mar", "Tck", "Pos", "Str"],
+        "blue": ["Dri", "Fir", "Pas", "Tec", "Agg", "Ant", "Bra", "Cmp", "Cnt", "Dec", "Agi", "Jum"]
+    },
+    "Inverted Wing Back - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Pas", "Tck", "Ant", "Dec", "Pos", "Tea"],
+        "blue": ["Fir", "Mar", "Tec", "Cmp", "Cnt", "OtB", "Agi"]
+    },
+    "Inverted Wing Back - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tck", "Cmp", "Dec", "Tea"],
+        "blue": ["Mar", "Tec", "Ant", "Cnt", "OtB", "Pos", "Vis", "Agi"]
+    },
+    "Inverted Wing Back - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tck", "Tec", "Cmp", "Dec", "OtB", "Tea", "Vis"],
+        "blue": ["Cro", "Dri", "Lon", "Mar", "Ant", "Cnt", "Fla", "Pos", "Agi"]
+    },
+    "Libero - Defend": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Fir", "Hea", "Mar", "Pas", "Tck", "Tec", "Dec", "Pos", "Tea", "Str"],
+        "blue": ["Ant", "Bra", "Cnt", "Sta"]
+    },
+    "Libero - Support": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Fir", "Hea", "Mar", "Pas", "Tck", "Tec", "Dec", "Pos", "Tea", "Str"],
+        "blue": ["Dri", "Ant", "Bra", "Cnt", "Vis", "Sta"]
+    },
+    "No-nonsense Centre Back - Defend": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Mar", "Tck", "Pos", "Str"],
+        "blue": ["Agg", "Ant", "Bra", "Cnt"]
+    },
+    "No-nonsense Centre Back - Stopper": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Tck", "Agg", "Bra", "Pos", "Str"],
+        "blue": ["Mar", "Ant", "Cnt"]
+    },
+    "No-nonsense Centre Back - Cover": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Mar", "Tck", "Ant", "Cnt", "Pos"],
+        "blue": ["Hea", "Bra", "Str"]
+    },
+    "Wide Centre Back - Defend": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Hea", "Mar", "Tck", "Pos", "Str"],
+        "blue": ["Dri", "Fir", "Pas", "Tec", "Agg", "Ant", "Bra", "Cnt", "Dec", "Wor", "Agi"]
+    },
+    "Wide Centre Back - Support": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Dri", "Hea", "Mar", "Tck", "Pos", "Str"],
+        "blue": ["Cro", "Fir", "Pas", "Tec", "Agg", "Ant", "Bra", "Cnt", "Dec", "OtB", "Wor", "Agi", "Sta"]
+    },
+    "Wide Centre Back - Attack": {
+        "key": ["Acc", "Pac", "Jum", "Cmp"],
+        "green": ["Cro", "Dri", "Hea", "Mar", "Tck", "OtB", "Sta", "Str"],
+        "blue": ["Fir", "Pas", "Tec", "Agg", "Ant", "Bra", "Cnt", "Dec", "Pos", "Wor", "Agi"]
+    },
+    "Wing Back - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Mar", "Tck", "Ant", "Pos", "Tea"],
+        "blue": ["Cro", "Dri", "Fir", "Pas", "Tec", "Cnt", "Dec", "OtB", "Agi", "Bal"]
+    },
+    "Wing Back - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Mar", "Tck", "OtB", "Tea"],
+        "blue": ["Fir", "Pas", "Tec", "Ant", "Cnt", "Dec", "Pos", "Agi", "Bal"]
+    },
+    "Wing Back - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Tck", "Tec", "OtB", "Tea"],
+        "blue": ["Fir", "Mar", "Pas", "Ant", "Cnt", "Dec", "Fla", "Pos", "Agi", "Bal"]
+    },
+    "Advanced Playmaker - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "OtB", "Tea", "Vis"],
+        "blue": ["Dri", "Ant", "Fla", "Agi"]
+    },
+    "Advanced Playmaker - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "OtB", "Tea", "Vis"],
+        "blue": ["Dri", "Ant", "Fla", "Agi"]
+    },
+    "Anchor - Defend": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Mar", "Tck", "Ant", "Cnt", "Dec", "Pos"],
+        "blue": ["Cmp", "Tea", "Str"]
+    },
+    "Attacking Midfielder - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Lon", "Pas", "Tec", "Ant", "Dec", "Fla", "OtB"],
+        "blue": ["Dri", "Cmp", "Vis", "Agi"]
+    },
+    "Attacking Midfielder - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Dri", "Fir", "Lon", "Pas", "Tec", "Ant", "Dec", "Fla", "OtB"],
+        "blue": ["Fin", "Cmp", "Vis", "Agi"]
+    },
+    "Ball Winning Midfielder - Defend": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Tck", "Agg", "Ant", "Tea"],
+        "blue": ["Mar", "Bra", "Cnt", "Pos", "Agi", "Str"]
+    },
+    "Ball Winning Midfielder - Support": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Tck", "Agg", "Ant", "Tea"],
+        "blue": ["Mar", "Pas", "Bra", "Cnt", "Agi", "Str"]
+    },
+    "Box to Box Midfielder - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Pas", "Tck", "OtB", "Tea"],
+        "blue": ["Dri", "Fin", "Fir", "Lon", "Tec", "Agg", "Ant", "Cmp", "Dec", "Pos", "Bal", "Str"]
+    },
+    "Carrilero - Support": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Fir", "Pas", "Tck", "Dec", "Pos", "Tea"],
+        "blue": ["Tec", "Ant", "Cmp", "Cnt", "OtB", "Vis"]
+    },
+    "Central Midfielder - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Tck", "Cnt", "Dec", "Pos", "Tea"],
+        "blue": ["Fir", "Mar", "Pas", "Tec", "Agg", "Ant", "Cmp"]
+    },
+    "Central Midfielder - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tck", "Dec", "Tea"],
+        "blue": ["Tec", "Ant", "Cmp", "Cnt", "OtB", "Vis"]
+    },
+    "Central Midfielder - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Dec", "OtB"],
+        "blue": ["Lon", "Tck", "Tec", "Ant", "Cmp", "Tea", "Vis"]
+    },
+        # Continuing from the previous positions...
+    "Deep Lying Playmaker - Defend": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "Tea", "Vis"],
+        "blue": ["Tck", "Ant", "Pos", "Bal"]
+    },
+    "Deep Lying Playmaker - Support": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "Tea", "Vis"],
+        "blue": ["Ant", "OtB", "Pos", "Bal"]
+    },
+    "Defensive Midfielder - Defend": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Tck", "Ant", "Cnt", "Pos", "Tea"],
+        "blue": ["Mar", "Pas", "Agg", "Cmp", "Str", "Dec"]
+    },
+    "Defensive Midfielder - Support": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Tck", "Ant", "Cnt", "Pos", "Tea"],
+        "blue": ["Fir", "Mar", "Pas", "Agg", "Cmp", "Dec", "Str"]
+    },
+    "Defensive Winger - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Tec", "Ant", "OtB", "Pos", "Tea"],
+        "blue": ["Cro", "Dri", "Fir", "Mar", "Tck", "Agg", "Cnt", "Dec"]
+    },
+    "Defensive Winger - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Pas", "Tec", "OtB", "Tea"],
+        "blue": ["Dri", "Fir", "Mar", "Pas", "Tck", "Agg", "Ant", "Cmp", "Cnt", "Dec", "Pos"]
+    },
+    "Enganche - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "Vis"],
+        "blue": ["Dri", "Ant", "Fla", "OtB", "Tea", "Agi"]
+    },
+    "Half Back - Defend": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Mar", "Tck", "Ant", "Cmp", "Cnt", "Dec", "Pos", "Tea"],
+        "blue": ["Fir", "Pas", "Agg", "Bra", "Jum", "Str"]
+    },
+    "Inside Forward - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Dri", "Fin", "Fir", "Tec", "OtB", "Agi"],
+        "blue": ["Lon", "Pas", "Ant", "Cmp", "Fla", "Vis", "Bal"]
+    },
+    "Inside Forward - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Dri", "Fin", "Fir", "Tec", "Ant", "OtB", "Agi"],
+        "blue": ["Lon", "Pas", "Cmp", "Fla", "Bal"]
+    },
+        # Continuing from the previous positions...
+    "Inverted Winger - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Pas", "Tec", "Agi"],
+        "blue": ["Fir", "Lon", "Cmp", "Dec", "OtB", "Vis", "Bal"]
+    },
+    "Inverted Winger - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Pas", "Tec", "Agi"],
+        "blue": ["Fir", "Lon", "Ant", "Cmp", "Dec", "Fla", "OtB", "Vis", "Bal"]
+    },
+    "Mezzala - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Pas", "Tec", "Dec", "OtB"],
+        "blue": ["Dri", "Fir", "Lon", "Tck", "Ant", "Cmp", "Vis", "Bal"]
+    },
+    "Mezzala - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Dri", "Pas", "Tec", "Dec", "OtB", "Vis"],
+        "blue": ["Fin", "Fir", "Lon", "Ant", "Cmp", "Fla", "Bal"]
+    },
+    "Raumdeuter - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fin", "Ant", "Cmp", "Cnt", "Dec", "OtB", "Bal"],
+        "blue": ["Fir", "Tec"]
+    },
+    "Regista - Support": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "Fla", "OtB", "Tea", "Vis"],
+        "blue": ["Dri", "Lon", "Ant", "Bal"]
+    },
+    "Roaming Playmaker - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tec", "Ant", "Cmp", "Dec", "OtB", "Tea", "Vis"],
+        "blue": ["Dri", "Lon", "Cnt", "Pos", "Agi", "Bal"]
+    },
+    "Segundo Volante - Support": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Mar", "Pas", "Tck", "OtB", "Pos"],
+        "blue": ["Fin", "Fir", "Lon", "Ant", "Cmp", "Cnt", "Dec", "Bal", "Str"]
+    },
+    "Segundo Volante - Attack": {
+        "key": ["Wor", "Sta", "Acc", "Pac"],
+        "green": ["Fin", "Lon", "Pas", "Tck", "Ant", "OtB", "Pos"],
+        "blue": ["Fir", "Mar", "Cmp", "Cnt", "Dec", "Bal"]
+    },
+    "Shadow Striker - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Dri", "Fin", "Fir", "Ant", "Cmp", "OtB"],
+        "blue": ["Pas", "Tec", "Cnt", "Dec", "Agi", "Bal"]
+    },
+    "Wide Midfielder - Defend": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Pas", "Tck", "Cnt", "Dec", "Pos", "Tea"],
+        "blue": ["Cro", "Fir", "Mar", "Tec", "Ant", "Cmp"]
+    },
+    "Wide Midfielder - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Pas", "Tck", "Dec", "Tea"],
+        "blue": ["Cro", "Fir", "Tec", "Ant", "Cmp", "Cnt", "OtB", "Pos", "Vis"]
+    },
+    "Wide Midfielder - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Fir", "Pas", "Dec", "Tea"],
+        "blue": ["Tck", "Tec", "Ant", "Cmp", "OtB", "Vis"]
+    },
+    "Wide Playmaker - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "Tea", "Vis"],
+        "blue": ["Dri", "OtB", "Agi"]
+    },
+    "Wide Playmaker - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Dri", "Fir", "Pas", "Tec", "Cmp", "Dec", "OtB", "Tea", "Vis"],
+        "blue": ["Ant", "Fla", "Agi"]
+    },
+    "Wide Target Forward - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Hea", "Bra", "Tea", "Jum", "Str"],
+        "blue": ["Cro", "Fir", "Ant", "OtB", "Bal"]
+    },
+    "Wide Target Forward - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Hea", "Bra", "OtB", "Jum", "Str"],
+        "blue": ["Cro", "Fin", "Fir", "Ant", "Tea", "Bal"]
+    },
+    "Winger - Support": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Tec", "Agi"],
+        "blue": ["Fir", "Pas", "OtB", "Bal"]
+    },
+    "Winger - Attack": {
+        "key": ["Acc", "Pac", "Sta", "Wor"],
+        "green": ["Cro", "Dri", "Tec", "Agi"],
+        "blue": ["Fir", "Pas", "Ant", "Fla", "OtB", "Bal"]
+    },
+    "Advanced Forward - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Dri", "Fir", "Tec", "Cmp", "OtB"],
+        "blue": ["Pas", "Ant", "Dec", "Wor", "Agi", "Bal", "Sta"]
+        
+    }, "Complete Forward - Support": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Dri", "Fir", "Hea", "Lon", "Pas", "Tec", "Ant", "Cmp", "Dec", "OtB", "Vis", "Agi", "Str"],
+        "blue": ["Tea", "Wor", "Bal", "Jum", "Sta"]
+    },
+    "Complete Forward - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Dri", "Fir", "Hea", "Tec", "Ant", "Cmp", "OtB", "Agi", "Str"],
+        "blue": ["Lon", "Pas", "Dec", "Tea", "Vis", "Wor", "Bal", "Jum", "Sta"]
+    },
+    "Deep Lying Forward - Support": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "OtB", "Tea"],
+        "blue": ["Ant", "Fla", "Vis", "Bal", "Str"]
+    },
+    "Deep Lying Forward - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Fir", "Pas", "Tec", "Cmp", "Dec", "OtB", "Tea"],
+        "blue": ["Dri", "Ant", "Fla", "Vis", "Bal", "Str"]
+    },
+    "False Nine - Support": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Dri", "Fir", "Pas", "Tec", "Cmp", "Dec", "OtB", "Vis", "Agi"],
+        "blue": ["Ant", "Fla", "Tea", "Bal"]
+    },
+    "Poacher - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Ant", "Cmp", "OtB"],
+        "blue": ["Fir", "Hea", "Tec", "Dec"]
+    },
+    "Pressing Forward - Defend": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Agg", "Ant", "Bra", "Dec", "Tea", "Wor", "Sta"],
+        "blue": ["Fir", "Cmp", "Cnt", "Agi", "Bal", "Str"]
+    },
+    "Pressing Forward - Support": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Agg", "Ant", "Bra", "Dec", "Tea", "Wor", "Sta"],
+        "blue": ["Fir", "Pas", "Cmp", "Cnt", "OtB", "Agi", "Bal", "Str"]
+    },
+    "Pressing Forward - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Agg", "Ant", "Bra", "OtB", "Tea", "Wor", "Sta"],
+        "blue": ["Fir", "Cmp", "Cnt", "Dec", "Agi", "Bal", "Str"]
+    },
+    "Target Forward - Support": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Hea", "Bra", "Tea", "Bal", "Jum", "Str"],
+        "blue": ["Fir", "Agg", "Ant", "Cmp", "Dec", "OtB"]
+    },
+    "Target Forward - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Hea", "Bra", "Cmp", "OtB", "Bal", "Jum", "Str"],
+        "blue": ["Fir", "Agg", "Ant", "Dec", "Tea"]
+    },
+    "Trequartista - Attack": {
+        "key": ["Acc", "Pac", "Fin"],
+        "green": ["Dri", "Fir", "Pas", "Tec", "Cmp", "Dec", "Fla", "OtB", "Vis"],
+        "blue": ["Ant", "Agi", "Bal"]
+    }
