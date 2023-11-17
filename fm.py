7df11967f81b540d804b4e8e423a67f19257d51f
Chris Lander <chrislander@gmail.com>
2023-11-17 22:47:04 +0000

    added requirements.txt and fixed bugs

--- fm.py
+++ fm.py
@@ -1,15 +1,29 @@
 import pandas as pd
+from io import StringIO
 
-
-def squad_html_to_dataframe(html_file):
-
-    tables = pd.read_html(html_file)
+def squad_html_to_dataframe(html_content):
+    # Use StringIO to create a file-like object from the HTML string
+    html_stream = StringIO(html_content)
+    
+    tables = pd.read_html(html_stream)
 
     df = tables[0]
 
+    # Apply fillna based on column data type
+    for column in df.columns:
+        if df[column].dtype == object:  # For object type columns, fill with empty string
+            df[column].fillna('', inplace=True)
+        else:
+            df[column].fillna(0.0, inplace=True)  # For numeric type columns, fill with 0.0
+
     return df
 
-html_file = 'exports/squad.html'
-df = squad_html_to_dataframe(html_file)
+with open('exports/squad.html', 'r', encoding='UTF-8') as file:
+    html_content = file.read()
+
+df = squad_html_to_dataframe(html_content)
 
 print(df)
+
+for column in df.columns:
+    print(column)
